f1c2f92594b828b049b329e38f520b97295d945b
Griffin Mareske <gmareske@gmail.com>
2018-09-25 17:48:38 +0200

    Implement id3 frame parsing
    Currently the parser works all the way up to any padding at the
    end of the tag and then hard stops. This grabs all the frames
    (I hope) but doesn't move up to the point where the mp3
    file starts. If users ever want to write out their own tags,
    figuring out where the mp3 player starts would be an internal
    feature that would be necessary.

--- src/Id3/Parser.hs
+++ src/Id3/Parser.hs
@@ -4,11 +4,13 @@ module Id3.Parser
 
 import Id3.Types
 
+
 import Control.Monad
 import Data.Binary.Get
 import Data.Bits
 import qualified Data.ByteString.Lazy as B
 import Data.Word
+import Text.Printf
 
 id3HeaderCode = B.unpack ("ID3" :: B.ByteString)
 
@@ -24,13 +26,17 @@ deserializeHeader = do
   minorVersion <- toInteger <$> getWord8
   flags <- getWord8
   unsync <- return $ testBit flags 7
-  extHeader <- return $ testBit flags 6 -- not supported
-  experiment <- return $ testBit flags 5 -- not supported
+  extHeader <- return $ testBit flags 6
+  experiment <- return $ testBit flags 5
   size <- toInteger <$> getWord32be
   if extHeader then deserializeExtHeader else return ()
-  return $ Id3 majorVersion minorVersion size extHeader experiment unsync []
+  return (Id3 majorVersion minorVersion size extHeader experiment unsync)
+    <*> deserializeFrames (fromIntegral size)
+  
 
--- | Parsing the extended header 
+-- | Parsing the extended header
+-- This procedure is only called from deserializeHeader if the
+-- extended header flag is set  
 deserializeExtHeader :: Get ()
 deserializeExtHeader = do
   headerSize <- fromIntegral <$> toInteger <$> getWord32be
@@ -41,11 +47,32 @@ deserializeExtHeader = do
     return ()
   else return ()
   return ()
-  
-readFrameHeader :: Get Frame
-readFrameHeader = do
-  frameId <- B.pack <$> replicateM 4 getWord8 -- better conversiont to ByteString
+
+deserializeFrames :: Int -> Get [Frame]
+deserializeFrames left = do
+  parseFrame left []
+--   replicateM 8 (parseOneFrame left [])
+
+parseFrame :: Int -> [Frame] -> Get [Frame]
+parseFrame left frames = do
+  if left > 0 then do
+    id <- B.pack <$> replicateM 4 getWord8
+    -- this check is done because often times id3 tags end with padding
+    -- which is done so id3 tags can be destructively expanded
+    if (head $ B.unpack id) == (0 :: Word8) then do
+      return $ frames -- we hit padding (id's bytes are always not null)
+      else do
+      size <- toInteger <$> getWord32be
+      flags <- getWord16be
+      -- labeled for debugging 
+      frameData <- label (printf "trying to get %d bytes with %d left, tag id=%s flags=%s" size left (show id) (show flags)) $ getLazyByteString (fromIntegral size)
+      parseFrame (left - (10 + (fromIntegral size))) $ (Frame id flags (fromIntegral size) frameData) : frames
+    else return $ frames
+
+parseOneFrame :: Int -> [Frame] -> Get Frame
+parseOneFrame x frames = do
+  id <- B.pack <$> replicateM 4 getWord8
   size <- toInteger <$> getWord32be
-  flags <- replicateM 2 getWord8
-  frameData <- B.pack <$> replicateM (fromIntegral size) getWord8 
-  return $ Frame frameId size frameData
+  flags <- getWord16be
+  frameData <- label (printf "trying to get %d bytes with %d left" size x) $ getLazyByteString (fromIntegral size)
+  return $ (Frame id flags (fromIntegral size) frameData)
--- src/Id3/Types.hs
+++ src/Id3/Types.hs
@@ -2,11 +2,15 @@ module Id3.Types
 where
 
 import qualified Data.Map as Map
+import Data.Word
 import qualified Data.ByteString.Lazy as B
+-- import Data.ByteString.Lazy.UTF8
 
 -- | Type representing an id3 frame per the spec
--- | Frame <id> <size in bytes> <data>
-data Frame = Frame B.ByteString Integer B.ByteString deriving (Show)
+-- | Frame <id> <flags> <size in bytes> <data>
+data Frame = Frame B.ByteString Word16 Int B.ByteString
+instance Show Frame where
+  show (Frame id flags size d) = "(Frame " ++ show id ++ ": size=" ++ show size ++ " flags="++ show flags ++")"
 
 -- | Data representing an id3 tag, with frames and information
 -- | from the header
