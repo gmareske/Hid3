391fab2aa02f80ea8aad2b88bc24429aad4b2aed
Griffin Mareske <gmareske@gmail.com>
2018-09-25 15:24:01 +0200

    Parse out the extended header if it exists
    Currently we throw out all the data. Also, this means this
    project as explicitly only supporting >Id3v2.3.0

--- src/Id3/Parser.hs
+++ src/Id3/Parser.hs
@@ -24,7 +24,28 @@ deserializeHeader = do
   minorVersion <- toInteger <$> getWord8
   flags <- getWord8
   unsync <- return $ testBit flags 7
-  compr <- return $ testBit flags 6
+  extHeader <- return $ testBit flags 6 -- not supported
+  experiment <- return $ testBit flags 5 -- not supported
   size <- toInteger <$> getWord32be
-  return $ Id3 majorVersion minorVersion size compr unsync []
+  if extHeader then deserializeExtHeader else return ()
+  return $ Id3 majorVersion minorVersion size extHeader experiment unsync []
+
+-- | Parsing the extended header 
+deserializeExtHeader :: Get ()
+deserializeExtHeader = do
+  headerSize <- fromIntegral <$> toInteger <$> getWord32be
+  extFlags <- getWord16be
+  paddingSize <- getWord32be
+  if testBit extFlags 15 then do
+    crc <- getWord32be
+    return ()
+  else return ()
+  return ()
   
+readFrameHeader :: Get Frame
+readFrameHeader = do
+  frameId <- B.pack <$> replicateM 4 getWord8 -- better conversiont to ByteString
+  size <- toInteger <$> getWord32be
+  flags <- replicateM 2 getWord8
+  frameData <- B.pack <$> replicateM (fromIntegral size) getWord8 
+  return $ Frame frameId size frameData
--- src/Id3/Types.hs
+++ src/Id3/Types.hs
@@ -2,17 +2,19 @@ module Id3.Types
 where
 
 import qualified Data.Map as Map
+import qualified Data.ByteString.Lazy as B
 
 -- | Type representing an id3 frame per the spec
-type Frame = Map.Map String String
+-- | Frame <id> <size in bytes> <data>
+data Frame = Frame B.ByteString Integer B.ByteString deriving (Show)
 
 -- | Data representing an id3 tag, with frames and information
 -- | from the header
 data Id3 = Id3 { version :: Integer
                , minorVersion :: Integer
                , id3Size :: Integer
-               , compressed :: Bool
+               , extHeader :: Bool
+               , experiment :: Bool
                , unsync :: Bool
                , frames :: [Frame]
                } deriving (Show) -- for debug
-
